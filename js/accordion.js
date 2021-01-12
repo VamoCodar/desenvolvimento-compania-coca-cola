0508ae1952265506e35d0b547e5f53638a25e5c4
adr <senhoraroldo@gmail.com>
2021-01-12 14:44:10 -0300

    penultima seçã ook

--- js/accordion.js
+++ js/accordion.js
@@ -14,29 +14,44 @@ accordionList.forEach((i) => {
 })
 
 
-//accordion cards
-
-
+//accordion cards branco
 
 const cardsWrapper1 = document.querySelector(".criterios__wrapper-1")
 const vejaMais1 = document.querySelector(".degrade__mobile-1 p")
+const degradeBranco = document.querySelector(".degrade__mobile-branco-1")
 
 function activeAccordion2() {
     cardsWrapper1.classList.toggle("active")
-    this.classList.toggle("active")
-
-}
-
-function conteudoP() {
+    vejaMais1.classList.toggle("active")
+    degradeBranco.classList.toggle("active")
     if (vejaMais1.classList.contains("active")) {
         vejaMais1.innerHTML = "Veja menos ▲";
-    } else if (!vejaMais1.classList.contains("active")) {
+    } else {
         vejaMais1.innerHTML = "Veja mais ▼"
     }
 
 }
 
-
-
 vejaMais1.addEventListener("click", activeAccordion2);
-vejaMais1.addEventListener("click", conteudoP);
+
+
+
+
+//accordion preto
+
+const cardsWrapper2 = document.querySelector(".criterios__wrapper-2")
+const vejaMais2 = document.querySelector(".criterios__wrapper-2 p.veja")
+const degradePreto = document.querySelector(".degrade__mobile-preto")
+
+function activeAccordionBlack() {
+    cardsWrapper2.classList.toggle("active")
+    degradePreto.classList.toggle("active")
+    vejaMais2.classList.toggle("active")
+    if (vejaMais2.classList.contains("active")) {
+        vejaMais2.innerHTML = "Veja menos ▲";
+    } else{
+        vejaMais2.innerHTML = "Veja mais ▼"
+    }
+}
+
+vejaMais2.addEventListener("click", activeAccordionBlack);
