c502b81d36448d64e9a7dcc2a108857b32a165a5
adr <senhoraroldo@gmail.com>
2021-01-12 11:26:45 -0300

    cards mobile

--- js/accordion.js
+++ js/accordion.js
@@ -1,5 +1,5 @@
+//accordion dos tiers
 const accordionList = document.querySelectorAll(".accordion__wrapper h5")
-
 
 function activeAccordion() {
     console.log(this);
@@ -12,3 +12,31 @@ accordionList.forEach((i) => {
     i.addEventListener("click",
         activeAccordion)
 })
+
+
+//accordion cards
+
+
+
+const cardsWrapper1 = document.querySelector(".criterios__wrapper-1")
+const vejaMais1 = document.querySelector(".degrade__mobile-1 p")
+
+function activeAccordion2() {
+    cardsWrapper1.classList.toggle("active")
+    this.classList.toggle("active")
+
+}
+
+function conteudoP() {
+    if (vejaMais1.classList.contains("active")) {
+        vejaMais1.innerHTML = "Veja menos ▲";
+    } else if (!vejaMais1.classList.contains("active")) {
+        vejaMais1.innerHTML = "Veja mais ▼"
+    }
+
+}
+
+
+
+vejaMais1.addEventListener("click", activeAccordion2);
+vejaMais1.addEventListener("click", conteudoP);
